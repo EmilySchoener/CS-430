c6dec4a2978b31a6f9301c168229a16e64708248
EmilySchoener <145405029+EmilySchoener@users.noreply.github.com>
2023-09-27 17:37:53 -0500

    Update Homework2.py
    Created dictionaries for training and verification data

--- Homework2/Homework2.py
+++ Homework2/Homework2.py
@@ -21,14 +21,43 @@ with open(file) as f:
 
 data = np.float64(data) #Change data type
 
-#Test
-print(data[0])
-print(data.shape)
+#add column of 1s for X0
+data = np.concatenate((np.ones((506,1), dtype=float), data), axis=1)
 
 #Split into training data and verification data
 train = data[:456]
 verify = data[456:]
 
-#Test to make sure it splits correctly
-#print(train[455])
-#print(verify[0])
+#--- Create Dictionaries ---
+tdata = dict()
+tdata['CRIM'] = train[:,1]
+tdata['ZN'] = train[:,2]
+tdata['INDUS'] = train[:,3]
+tdata['CHAS'] = train[:,4]
+tdata['NOX'] = train[:,5]
+tdata['RM'] = train[:,6]
+tdata['AGE'] = train[:,7]
+tdata['DIS'] = train[:,8]
+tdata['RAD'] = train[:,9]
+tdata['TAX'] = train[:,10]
+tdata['PTRATIO'] = train[:,11]
+tdata['B'] = train[:,12]
+tdata['LSTAT'] = train[:,13]
+tdata['MEDV'] = train[:,14]
+
+vdata = dict()
+vdata['CRIM'] = verify[:,1]
+vdata['ZN'] = verify[:,2]
+vdata['INDUS'] = verify[:,3]
+vdata['CHAS'] = verify[:,4]
+vdata['NOX'] = verify[:,5]
+vdata['RM'] = verify[:,6]
+vdata['AGE'] = verify[:,7]
+vdata['DIS'] = verify[:,8]
+vdata['RAD'] = verify[:,9]
+vdata['TAX'] = verify[:,10]
+vdata['PTRATIO'] = verify[:,11]
+vdata['B'] = verify[:,12]
+vdata['LSTAT'] = verify[:,13]
+vdata['MEDV'] = verify[:,14]
+#--- End of Dictionary Creation ---
