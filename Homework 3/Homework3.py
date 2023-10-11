1bf4faa1b2800e2b1fb487d4fda795e7bad2a57a
JoshuaSVU <115957792+JoshuaSVU@users.noreply.github.com>
2023-10-11 16:16:02 -0500

    Data collection
    managed the data and parsed it into train/verify with an 80/20 split

--- Homework 3/Homework3.py
+++ Homework 3/Homework3.py
@@ -2,9 +2,15 @@
 import numpy as np
 import sigmoid as sigmoid
 
-file = "iris.data"
+file = "iris.txt"
 
 data = []
+train = []
+verify = []
+setosa = []
+versicolor = []
+virginica = []
+
 
 # Read in data from file
 with open(file) as f:
@@ -14,11 +20,30 @@ with open(file) as f:
         line = line.strip()
         l = line.split(',')
         if l[4] == "Iris-setosa":
-            l[4] = 1
+            #l[4] = 1
+            setosa.append(l)
         elif l[4] == "Iris-versicolor":
-            l[4] = 2
+            #l[4] = 2
+            versicolor.append(l)
         elif l[4] == "Iris-virginica":
-            l[4] = 3
-        data.append(l)
-np.random.shuffle(data)
-print(data[0])
+            #l[4] = 3
+            virginica.append(l)
+#data shuffled to separate into train/verify
+np.random.shuffle(setosa)
+np.random.shuffle(versicolor)
+np.random.shuffle(virginica)
+
+#train verify split 80% 20%
+train.extend(setosa[:40])
+train.extend(versicolor[:40])
+train.extend(virginica[:40])
+
+verify.extend(setosa[-10:])
+verify.extend(versicolor[-10:])
+verify.extend(virginica[-10:])
+
+print(train[0])
+print(verify[0])
+print(setosa[0])
+print(versicolor[0])
+print(virginica[0])
